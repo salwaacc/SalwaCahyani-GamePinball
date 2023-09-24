53f99b6d01b76f6f70a2f36c8183e832614194ac
salwaacc <salwac707@gmail.com>
2023-09-24 22:05:28 +0700

    Ball, Paddle, Bumper

---
+++ Assets/Script/PaddleController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public KeyCode input;
+    private HingeJoint hinge;
+    private float targetPressed;
+    private float targetRelease;
+ 
+    // Start is called before the first frame update
+    private void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        targetPressed = hinge.limits.max;
+        targetRelease = hinge.limits.min;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        ReadInput();
+    }
+
+    private void ReadInput(){            
+        JointSpring jointSpring = hinge.spring;
+
+        if(Input.GetKey(input)){
+            jointSpring.targetPosition = targetPressed;
+
+        } else {
+            jointSpring.targetPosition = targetRelease;
+        }
+       
+       hinge.spring = jointSpring;
+    }
+}
